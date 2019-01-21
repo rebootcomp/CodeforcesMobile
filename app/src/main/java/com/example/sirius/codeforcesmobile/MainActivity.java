2b39ba73aa1a10fd5a933c638d4cf29e0646133e
rebootcomp <x.dovakin@yandex.ru>
2019-01-22 02:51:13 +0300

    бд для новостей и классы задач

--- app/src/main/java/com/example/sirius/codeforcesmobile/MainActivity.java
+++ app/src/main/java/com/example/sirius/codeforcesmobile/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.sirius.codeforcesmobile;
 
 import android.annotation.SuppressLint;
 import android.app.FragmentTransaction;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.TimeZone;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -25,14 +31,21 @@ import com.example.sirius.codeforcesmobile.Fragments.NotificationFragment;
 import com.example.sirius.codeforcesmobile.Fragments.ProfileFragment;
 import com.example.sirius.codeforcesmobile.Fragments.SearchFragment;
 import com.example.sirius.codeforcesmobile.connectionAPI.ApiListResponse;
+import com.example.sirius.codeforcesmobile.connectionAPI.BlogResult;
 import com.example.sirius.codeforcesmobile.connectionAPI.UserResult;
 import com.example.sirius.codeforcesmobile.connectionAPI.User;
 import com.example.sirius.codeforcesmobile.connectionAPI.UserInterface;
 import com.example.sirius.codeforcesmobile.connectionAPI.UserResult;
 import com.example.sirius.codeforcesmobile.connectionAPI.funcsAPI;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -88,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint({"RestrictedApi", "ResourceType"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,8 +128,8 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
             List<UserResult> userResult = null;
             userResult = (List<UserResult>) users;
-
-            db.execSQL("INSERT INTO users VALUES ('"+userResult.get(0).getRank()+"','" + userResult.get(0).getHandle() + "', '" + userResult.get(0).getFirstName().toString() + "','" + userResult.get(0).getLastName().toString() + "','" + userResult.get(0).getRating().toString() + "','" + userResult.get(0).getMaxRating().toString() + "','" + userResult.get(0).getMaxRank().toString() + "','"+userResult.get(0).getContribution().toString()+"','"+userResult.get(0).getFriendOfCount().toString()+"');");
+            /**доделать ввод в бд**/
+            // db.execSQL("INSERT INTO users VALUES ('"+userResult.get(0).getRank()+"','" + userResult.get(0).getHandle() + "', '" + userResult.get(0).getFirstName().toString() + "','" + userResult.get(0).getLastName().toString() + "','" + userResult.get(0).getRating().toString() + "','" + userResult.get(0).getMaxRating().toString() + "','" + userResult.get(0).getMaxRank().toString() + "','"+userResult.get(0).getContribution().toString()+"','"+userResult.get(0).getFriendOfCount().toString()+"');");
             Log.d("RETROFIT", userResult.toString());
             // insert into LOCAL DB (variant1)
             // insert into fragment in bundle(variant2)
@@ -125,15 +139,45 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        String[] newsList = {"64685", "64613", "64495"};
+       // for (int i = 0; i < 3; i++) {
+            api.getBlog("64685", blog -> {
+                BlogResult blogResult = (BlogResult) blog;
+
+                SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+
+                String Title = Jsoup.parse(blogResult.getTitle()).text();
+                String content = Jsoup.parse(blogResult.getContent()).text();
+                String author = blogResult.getAuthorHandle();
+
+                long millis = blogResult.getCreationTimeSeconds() * 1000;
+                Date date = new Date(millis);
+                SimpleDateFormat sdf = new SimpleDateFormat("EEEE,MMMM d,yyyy h:mm,a", Locale.ENGLISH);
+                sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+                String Date = sdf.format(date);
+                //content = content.replace("'", " ");
+                ContentValues values = new ContentValues();
+                //title TEXT,author TEXT,date TEXT,content TEXT
+                values.put("title", Title);
+                values.put("author", author);
+                values.put("date", Date);
+                values.put("content", content);
+                db.insert("blogs",null,  values);
+                //db.execSQL("INSERT INTO blogs VALUES ('"+Title+"','"+author+"','"+date+"','"+content+"');'");
+               /* Cursor query = db.rawQuery("SELECT * FROM blogs;", null);
+                query.moveToLast();
+                Toast.makeText(getApplicationContext(), query.getString(2) , Toast.LENGTH_LONG).show();*/
+            });
 
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        navigation.getMenu().getItem(0).setChecked(false);
-        navigation.getMenu().getItem(2).setChecked(true);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+            BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+            navigation.getMenu().getItem(0).setChecked(false);
+            navigation.getMenu().getItem(2).setChecked(true);
+            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
-    }
+        }
+  //  }
 
     private void callAfterRetro() {
 
