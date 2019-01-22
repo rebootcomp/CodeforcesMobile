6cd46651dcde6dd30ba632857d51306b891afac2
Marselka974 <marsel.ahmetov974@gmail.com>
2019-01-22 12:18:02 +0300

    Сделана функция логина

--- app/src/main/java/com/example/sirius/codeforcesmobile/LoginActivity.java
+++ app/src/main/java/com/example/sirius/codeforcesmobile/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.sirius.codeforcesmobile.connectionAPI.UserResult;
 import com.example.sirius.codeforcesmobile.connectionAPI.funcsAPI;
@@ -27,18 +28,29 @@ public class LoginActivity extends AppCompatActivity {
 
         String login = String.valueOf(editTextLogin.getText());
         funcsAPI api = new funcsAPI();
+
         api.getUsers(login, users -> {
-            SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-            List<UserResult> userResult = null;
-            userResult = (List<UserResult>) users;
-            db.execSQL("INSERT INTO users VALUES ('"+userResult.get(0).getRank()+"','" + userResult.get(0).getHandle() + "', '" + userResult.get(0).getFirstName() + "','" + userResult.get(0).getLastName() + "','" + userResult.get(0).getRating().toString() + "','" + userResult.get(0).getMaxRating().toString() + "','" + userResult.get(0).getMaxRank() + "','"+userResult.get(0).getContribution().toString()+"','"+userResult.get(0).getFriendOfCount().toString()+"');'");
-            Log.d("RETROFIT", userResult.toString());
-            db.close();
+            if(users!=null) {
+                SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+                List<UserResult> userResult = null;
+                userResult = (List<UserResult>) users;
+                /**доделать ввод в бд**/
+                db.execSQL("INSERT INTO users VALUES ('" + userResult.get(0).getRank() + "','" + userResult.get(0).getHandle() + "', '" + userResult.get(0).getFirstName() + "','" + userResult.get(0).getLastName() + "','" + userResult.get(0).getRating().toString() + "','" + userResult.get(0).getMaxRating().toString() + "','" + userResult.get(0).getMaxRank() + "','" + userResult.get(0).getContribution().toString() + "','" + userResult.get(0).getFriendOfCount().toString() + "');");
+                Log.d("RETROFIT", userResult.toString());
+                // insert into LOCAL DB (variant1)
+                // insert into fragment in bundle(variant2)
+                // insert into sherperf (variant3)
+
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }else{
+                Toast.makeText(getApplicationContext(),"Пользователь введен не верно",Toast.LENGTH_SHORT).show();
+            }
         });
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+
+
     }
 
     public void onEnterWithout(View view) {
